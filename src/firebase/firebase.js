ae7caf831489abd505fb999bbdae3f0ca72f482b
WeiKee <weikee070194@gmail.com>
2017-12-14 18:07:09 +0800

    update firebase with several method

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,19 +13,124 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database.ref().set({
-    name: 'Wei Kee',
-    age: 23,
-    isSingle: true,
-    location: {
-        city: 'Johor Bahru',
-        country: 'Malaysia'
-    }
-}).then(() => {
-    console.log('Data is saved!');
-}).catch((e) => {
-    console.log('Failed', e);
+// update data
+database.ref('notes/-L0JR24oogioenze_dOu').update({
+    body: 'Buy Food'
 });
+
+// this is how firebase data should looks like
+database.ref('expenses').push({
+    description: 'A',
+    note: 'Note A',
+    amount: 1,
+    createdAt: 2017
+});
+
+database.ref('expenses').push({
+    description: 'B',
+    note: 'Note B',
+    amount: 2,
+    createdAt: 2018
+});
+
+database.ref('expenses').push({
+    description: 'C',
+    note: 'Note C',
+    amount: 3,
+    createdAt: 2019
+});
+
+// database.ref('notes').push({
+//     title: 'C',
+//     body: 'React'
+// });
+
+// const firebaseNotes = {
+//     notes: {
+//         a: {
+//             title: 'A',
+//             body: 'Body A'
+//         },
+//         b: {
+//             title: 'B',
+//             body: 'Body B'
+//         }
+//     }
+// };
+
+// subscription method
+// const onValueChange = database.ref().on('value', (snapshot) => {
+//     console.log(snapshot.val());
+// }, (e) => {
+//     console.log('Error with data fetching', e);
+// });
+
+// setTimeout(() => {
+//     database.ref('stressLevel').set(10);
+// }, 3500);
+
+// unsubscribe by using off method
+// setTimeout(() => {
+//     database.ref().off('value', onValueChange);
+// }, 7000);
+
+// setTimeout(() => {
+//     database.ref('stressLevel').set(30);
+// }, 10500);
+
+// database.ref('location')
+//     .once('value')
+//     .then((snapshot) => {
+//         const val = snapshot.val();
+//         console.log(val);
+//     })
+//     .catch((e) => {
+//         console.log('Error fetching data', e);
+//     });
+
+// 删除data 1
+// database.ref('isSingle')
+//         .remove()
+//         .then(() => {
+//             console.log('removed');
+//         }).catch((e) => {
+//             console.log('Error', e);
+//         });
+// 删除data 2
+// database.ref('age').set(null);
+
+// database.ref().set({
+//     stressLevel: 6,
+//     job: {
+//         title: 'Software Engineer',
+//         company: 'Google'
+//     },
+//     location: {
+//         country: 'England'
+//     }
+// });
+
+// update data
+// database.ref().update({
+//     stressLevel: 9,
+    // update object 时候要使用/
+//     'job/company': 'Amazon',
+//     'location/city': 'Seattle'
+// });
+
+// database.ref().set({
+//     name: 'Wei Kee',
+//     age: 23,
+//     isSingle: true,
+//     location: {
+//         city: 'Johor Bahru',
+//         country: 'Malaysia'
+//     }
+// }).then(() => {
+//     console.log('Data is saved!');
+// }).catch((e) => {
+//     console.log('Failed', e);
+// });
 
 // database.ref('age').set(24);
 // database.ref('location/city').set('New York');
@@ -38,11 +143,11 @@ database.ref().set({
 // switch rules to be open
 // make sure then runs
 
-database.ref('attr/height').set({
-    height: 173,
-    weight: 55
-}).then(() => {
-    console.log('Success');
-}).catch((e) => {
-    console.log('Failed');
-});
+// database.ref('attr/height').set({
+//     height: 173,
+//     weight: 55
+// }).then(() => {
+//     console.log('Success');
+// }).catch((e) => {
+//     console.log('Failed');
+// });
