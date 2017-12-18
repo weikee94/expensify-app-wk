cbaadb0d01d87733ee08113ac6515279b4147332
WeiKee <weikee070194@gmail.com>
2017-12-18 11:48:02 +0800

    fetching data

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -50,3 +50,30 @@ export const editExpense = (id, updates) => ({
   id,
   updates
 });
+
+// SET_EXPENSES
+export const setExpenses = (expenses) => ({
+  type: 'SET_EXPENSES',
+  expenses
+});
+
+// asynchronous action to fetch data and dispatch set expenses
+// export const startSetExpenses;
+// 1. Fetch all expense data once
+// 2. Parse that data into an array
+// 3. Dispatch SET_EXPENSES
+export const startSetExpenses = () => {
+  return (dispatch) => {
+    return database.ref('expenses').once('value').then((snapshot) => {
+      const expenses = [];
+       snapshot.forEach((childSnapshot) => {
+        expenses.push({
+          id: childSnapshot.key,
+          ...childSnapshot.val()
+        });
+       });
+       dispatch(setExpenses(expenses));
+    });
+  };
+};
+
