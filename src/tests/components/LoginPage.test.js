bec3799b585f69a10c95fde293e29062f4247f27
WeiKee <weikee070194@gmail.com>
2017-12-20 21:01:35 +0800

    auth reducer with test case example

--- src/tests/components/LoginPage.test.js
+++ src/tests/components/LoginPage.test.js
@@ -3,12 +3,13 @@ import { shallow } from 'enzyme';
 import { LoginPage } from '../../components/LoginPage';
 
 test('should correctly render LoginPage', () => {
-    const wrapper = shallow();
+  const wrapper = shallow(<LoginPage />);
+  expect(wrapper).toMatchSnapshot();
 });
 
 test('should call startLogin on button click', () => {
-    const startLogin = jest.fn();
-    const wrapper = shallow(<Login startLogin={startLogin} />);
-    wrapper.find('button').simulate('click');
-    expect(startLogin).toHaveBeenCalled();
+  const startLogin = jest.fn();
+  const wrapper = shallow(<LoginPage startLogin={startLogin} />);
+  wrapper.find('button').simulate('click');
+  expect(startLogin).toHaveBeenCalled();
 });
