1a9b135a42b6b93709f5c88d870435746f0dca5c
WeiKee <weikee070194@gmail.com>
2017-12-20 21:42:57 +0800

    private routes setup

--- src/app.js
+++ src/app.js
@@ -1,3 +1,57 @@
+// My version
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import AppRouter, { history } from './routers/AppRouter';
+// import configureStore from './store/configureStore';
+// import { startSetExpenses } from './actions/expenses';
+// import { login, logout } from './actions/auth';
+// import getVisibleExpenses from './selectors/expenses';
+// import 'normalize.css/normalize.css';
+// import './styles/styles.scss';
+// import 'react-dates/lib/css/_datepicker.css';
+// import { firebase } from './firebase/firebase';
+// import './playground/promises';
+
+// const store = configureStore();
+
+// const jsx = (
+//   <Provider store={store}>
+//     <AppRouter />
+//   </Provider>
+// );
+
+// let hasRendered = false;
+// const renderApp = () => {
+//   if(!hasRendered) {
+//     ReactDOM.render(jsx, document.getElementById('app'));
+//     hasRendered = true;
+//   }
+// };
+
+// ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+// firebase.auth().onAuthStateChanged((user) => {
+//   if(user) {
+//     store.dispatch(login(user.id));
+//     store.dispatch(startSetExpenses()).then(() => {
+//       renderApp();
+//       if(history.location.pathname === '/') {
+//         history.push('/dashboard');
+//       }
+//     });
+//   } else {
+//     store.dispatch(logout());
+//     renderApp();
+//     history.push('/');
+//   }
+// });
+
+
+
+
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
@@ -10,19 +64,16 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import { firebase } from './firebase/firebase';
-import './playground/promises';
 
 const store = configureStore();
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
   </Provider>
 );
-
 let hasRendered = false;
 const renderApp = () => {
-  if(!hasRendered) {
+  if (!hasRendered) {
     ReactDOM.render(jsx, document.getElementById('app'));
     hasRendered = true;
   }
@@ -31,11 +82,11 @@ const renderApp = () => {
 ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
-  if(user) {
-    store.dispatch(login(user.id));
+  if (user) {
+    store.dispatch(login(user.uid));
     store.dispatch(startSetExpenses()).then(() => {
       renderApp();
-      if(history.location.pathname === '/') {
+      if (history.location.pathname === '/') {
         history.push('/dashboard');
       }
     });
