1703c38f29f199ef2015f605bc7ac5abff2493c4
WeiKee <weikee070194@gmail.com>
2017-12-20 16:29:41 +0800

    logging out with test case

--- src/tests/components/LoginPage.test.js
+++ src/tests/components/LoginPage.test.js
@@ -5,3 +5,10 @@ import { LoginPage } from '../../components/LoginPage';
 test('should correctly render LoginPage', () => {
     const wrapper = shallow();
 });
+
+test('should call startLogin on button click', () => {
+    const startLogin = jest.fn();
+    const wrapper = shallow(<Login startLogin={startLogin} />);
+    wrapper.find('button').simulate('click');
+    expect(startLogin).toHaveBeenCalled();
+});
