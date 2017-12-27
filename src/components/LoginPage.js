16474abbad599e0f3da5ca9b3c06c226f980067b
WeiKee <weikee070194@gmail.com>
2017-12-27 17:40:00 +0800

    header and button styling

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,9 +5,9 @@ import { startLogin } from '../actions/auth';
 export const LoginPage = ({ startLogin }) => (
     <div className="box-layout">
         <div className="box-layout__box">
-            <h1 className="box-layout__title">Expensify App</h1>
+            <h1 className="box-layout__title">Expensify</h1>
             <p>It's time to get your expenses under control.</p>
-            <button onClick={startLogin}>Login</button>
+            <button onClick={startLogin} className="button">Login With Google</button>
         </div>
     </div>
 );
